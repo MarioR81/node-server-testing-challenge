27b708d2bd83f6e7558939e2ae28c41d1bccb4d6
apple <apple@apples-MacBook-Pro-2.local>
2020-03-26 21:40:18 -0700

    finished

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+
+const server = require('../api/server');
+
+
+describe('User Router.js', function() {
+    
+    describe('GET /', function() {
+        // it('should return 400 for users that are not auth', function() {
+        //     return request(server)
+        //     .get('/api/auth/register')
+        //     .expect(200);
+        // })
+
+        // it('should return welcome string', function() {
+        //     return request(server).get('/api/auth/register')
+        //     .then(res => {
+        //         expect(res.type).toMatch(/json/i)
+        //     })
+        // });
+
+        // it('should return with {api: up}', function() {
+        //     return request(server).get('/')
+        //     .then(res => {
+        //         expect(res.body.api).toBe('up');
+        //     })
+        // })
+ 
+    })
+})
